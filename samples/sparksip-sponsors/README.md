9deeb4f9737517dfa1af3db082b513b6f5982bcb
FunMu <funmu@users.noreply.github.com>
2020-03-14 15:25:33 -0700

    update for 2020

--- samples/sparksip-sponsors/README.md
+++ samples/sparksip-sponsors/README.md
@@ -1,14 +1,31 @@
 # README.md
 
-## Tools
+Small collection of custom script to process the Sparksip Sponsors list to create 
 
-### Azuqua
- Try out Azuqua to build up convesion tools.
+## Files and Folders
+
+ * [SparkSIP Convertor](xformSparkSip.js) uses nodejs for conversion of input JSON to HTML
+
+ * [Inputs](./inputs) Folder
+ * [Outputs](./output) Folder
+ * [Templates Folder](./templates)
+
+
+## Steps
+Steps to get the projects
+
+1. Get sponsorships from Business owners - run a Google Forms inquiry (Feb)
+2. Export the JSON view from sruvey results (data as array using "Add-ons / Export Sheet Data")
+3. Ensure some key fields are present - Title, email, etc.
+4. Store this exported JSON file in the inputs folder
+5. run "make output" to generate the output
+6. zip up the output files and send it to Sparksip Organizers
+
+### Sparksip JSON to HTML files
+ Convert the input SPARKSIP JSON files to HTML to post on the sparksip.org website
 
  Concluded that I Cannot do this conversion easily after spending 45 minutes on it.
 
- ## JSON to HTML conversion using nodejs
- * [SparkSIP Convertor](xformSparkSip.js)
 
  Run the converter code as follows to generate the required output html
  ```
--- samples/sparksip-sponsors/xformSparksip.js
+++ samples/sparksip-sponsors/xformSparksip.js
@@ -8,23 +8,56 @@ var fVerbose = false;
 var fs = require('fs');
 var fileToProcess = './inputs/1sponsor.json'; // sample file for testing purposes
 var templateForHTML = "templates/templateForProjects.html";
-var outputDirectory = "output/2019/";
+var outputDirectoryRoot = "output/";
 var outputIndexStart = 201900; // some random number. use a uniquefier in the future
+
+
+const input_output_key_aliases = {
+
+	"Timestamp" : "Timestamp",
+	"ProjectId" : "ProjectId",
+
+	"Email Address" : "ContactEmail",
+	"ContactTitle" : "ContactTitle",
+	"Name of your Organization" : "Organization",
+	"OrganizationWebsite" : "OrganizationWebsite",
+	"Who is the best contact  (name + email) if not you?": "ContactName",
+	
+	"Length of Internship (in number of weeks)" : "Length",
+	"Number of hours per week": "HoursPerWeek",
+	"Can you pay an optional stipend of (max of $500 per intern)?": "Stipend",
+	
+	"Project Title (6-8 words)" : "ProjectTitle",
+	"Describe the project (3-5 sentences)": "Description",
+	"Qualifications you are looking for (3-5 sentences)": "Qualifications",
+
+	"Is there a Scientific or Business Research component?": "HasResearch",
+	"Describe the Research Component (3-5 sentences)": "ResearchDescription"
+};
 
 // --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // Process Command line to get parameters for processing
 
-// console.log( process.argv);
 if (process.argv.length > 2) {
 
-	// 2nd argument is the input data file
-	fileToProcess = process.argv[2];
+	// 2nd argument is the year for which we are processing this input
+	outputDirectory = outputDirectoryRoot + process.argv[2] + "/";
+	outputIndexStart = parseInt(process.argv[2])*100;
+} else {
+	outputDirectory = outputDirectoryRoot + "Unknown" + "/"
 }
 
 if (process.argv.length > 3) {
 
-	// 3nd argument is the template HTML data file
-	templateForHTML = process.argv[3];
+	// 3rd argument is the input data file
+	fileToProcess = process.argv[3];
+}
+
+
+if (process.argv.length > 4) {
+
+	// 4th argument is the template HTML data file
+	templateForHTML = process.argv[4];
 }
 
 // --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -60,13 +93,34 @@ function printProjectsJSON( p, i) {
 }
 
 /*
+ * Maps the alias long name keys to short structured key names
+ * 
+ *
+ * @param{object} p - input object with long name keys from survey results
+ * @param{i} integer - integer index for the object
+ *		for processing data
+ * 
+ * @return{object} structured object with short key names
+ */
+function deAliasKeyNames( p, i) 
+{
+	const keysInput = Object.keys(p);
+	var outputObject = {};
+	keysInput.forEach( (k, i) => {
+		outputObject[ input_output_key_aliases[k]] = p[k];
+	});
+
+	return outputObject;
+}
+
+/*
  * Formats the HTML information by applying the parameters 
  * 
  *
  * @param{string} htmlTemplateFile - reference to the template file
  * @param{parameters} array of objects - collection of parameters used 
  *		for processing data
-* 
+ * 
  * @return{object} formatted HTML as a textual string
  */
 function FormatModelInfo( htmlTemplateFile, parameters)
@@ -105,26 +159,29 @@ function formatJSON2Html( p, i) {
 	console.log("\n --- FORMATTING JSON PROJECT definition to HTML ---");
 	console.log( "[%d] %s\n", i, p.ProjectTitle);
 	p["ProjectId"] = outputIndexStart + i;
-	var fh = FormatModelInfo( templateForHTML, p);
+	var formattedHtml = FormatModelInfo( templateForHTML, p);
 
-	// console.log( fh);
+	// console.log( formattedHtml);
 	var fileNameToOutput = outputDirectory + p["ProjectId"] + ".html";
 	console.log("\tSaving HTML for [%d] into file [%s]", i, fileNameToOutput);
-	fs.writeFileSync( fileNameToOutput, fh);
+	fs.writeFileSync( fileNameToOutput, formattedHtml);
 }
+
 
 // --=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // Load up the input data file
-var projectsJSON = require( fileToProcess);
+var inputProjectsJSON = require( fileToProcess);
 
 if (fVerbose) {
 	console.log("\n\n ------------------------------------");
-	console.log( projectsJSON);
+	console.log( inputProjectsJSON);
 }
 
 // Iterate through each project and output the JSON in pretty form
 // projectsJSON.forEach( printProjectsJSON);
 
+var projectsJSON = inputProjectsJSON.map( deAliasKeyNames);
+console.log( projectsJSON);
 
 // Iterate through each project, format the HTML and spit it out
 projectsJSON.forEach( formatJSON2Html);
